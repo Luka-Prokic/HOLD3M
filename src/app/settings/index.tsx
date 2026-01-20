1abac0ca27f11e0ed649afdccda8c0d48274d7c1
Luka Prokic <luka22prokic@gmail.com>
2026-01-20 04:35:53 +0100

    Title name chnage

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -21,7 +21,7 @@ export default function Page() {
                     <AceButton title="LIGHT" onPress={() => setTheme("light")} themeType="tint" />
 
                     <AccentTintButton title="Crimson" tint={"#FF8A78"} accent={"#7A3E2A"} />
-                    <AccentTintButton title="Vapor" tint={"#B7AEFF"} accent={"#5747E5"} />
+                    <AccentTintButton title="Frost" tint={"#B7AEFF"} accent={"#5747E5"} />
                     <AccentTintButton title="Mono" tint={"#8C867C"} accent={"#2A2723"} />
                 </View>
             </ScreenContent>
